4be555ba6126bef70e6782290f379f72e060121f
Tudor Zaharia <zahariatm@yahoo.com>
2024-05-14 14:11:17 +0300

    Added some more comments.

--- Client.h
+++ Client.h
@@ -27,27 +27,61 @@ class Client {
     std::string get_error_message(const std::string &response);
 
 
+    /**
+     * Gets stdin input corresponding to a new user and sends
+     * a POST request to the server, asking to register him.
+     */
     void manage_register();
 
 
+    /**
+     * Gets stdin input with user credentials and sends a POST request
+     * to the server, asking to log the user in. Updates the session
+     * cookies in case of success.
+     */
     void manage_login();
 
 
+    /**
+     * Sends a GET request to the server, asking to log out the current user
+     * Clears the cookies and the jwt in case of success.
+     */
     void manage_logout();
 
 
+    /**
+     * Sends a GET request to the server, asking for access to the library
+     * for the currently logged in user. Updates the jwt in case of success.
+     */
     void manage_enter_library();
 
 
+    /**
+     * Gets stdin input containing details of a new book. Sends POST request
+     * to the server, asking to add the new book to the library.
+     */
     void manage_add_book();
 
 
+    /**
+     * Sends a GET request to the server, asking for a JSON object containing
+     * all books that the currently logged in user has in the library.
+     */
     void manage_get_books();
 
 
+    /**
+     * Gets stdin input containing the id of a book and sends GET request
+     * to the server, asking for the details associated with the book.
+     */
     void manage_get_book();
 
 
+    /**
+     * Gets stdin input containing the id of a book and sends DELETE request
+     * to the server, asking for removal of the book from the library of the
+     * currently logged in user.
+     */
     void manage_delete_book();
 
 
